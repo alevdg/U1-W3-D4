c9a056e18f5b309ca7bf771ea09307aafd70f779
alevdg <alessandravanessadg@gmail.com>
2023-06-08 15:50:02 +0200

    Esercizio 08/06

--- scripts.js
+++ scripts.js
@@ -1,34 +1,42 @@
-let tabellone = document.getElementById("tabellone")
-let numeroCelle = 76
-let numeriEstratti = []
+let tabellone = document.getElementById("tabellone");
+let numeroCelle = 76;
+let numeriEstratti = [];
+let cellePerRiga = 13;
 
-//Creazione tabella 
-for (let i=0; i<numeroCelle; i++){
+// Creazione tabella
+for (let i = 0; i < numeroCelle; i++) {
+    if (i % cellePerRiga === 0) {
+        // Crea una nuova riga ogni 8 celle
+        let riga = document.createElement("tr");
+        tabellone.appendChild(riga);
+    }
+    let rigaCorrente = tabellone.lastChild;
     let cella = document.createElement("td");
     cella.innerText = i + 1;
-    cella.id = "cella" + (i+1);
-    let riga = document.createElement("tr");
-    riga.appendChild(cella);
-    tabellone.appendChild(riga);
+    cella.id = "cella" + (i + 1);
+    rigaCorrente.appendChild(cella);
 }
 
-//funzione estrazione numeri
-function estraiNumero(){
-    if(numeriEstratti.length===numeroCelle){
-        document.getElementById("numeroEstratto").innerText ==="Tutti i numeri sono stati estratti";
+
+// Funzione estrazione numeri
+function estraiNumero() {
+    if (numeriEstratti.length === numeroCelle) {
+        document.getElementById("numeroEstratto").innerText = "Tutti i numeri sono stati estratti";
         return;
     }
 
     let numeroEstratto;
-    do{
-        numeroEstratto = Math.floor(Math.random()*numeroCelle)+1;}
-        while (numeriEstratti.includes(numeroEstratto));
+    do {
+        numeroEstratto = Math.floor(Math.random() * numeroCelle) + 1;
+    } while (numeriEstratti.includes(numeroEstratto));
 
-        numeriEstratti.push(numeroEstratto);
+    numeriEstratti.push(numeroEstratto);
 
-        document.getElementById("numeroEstratto").innerText = "Numero Estratto:"+numeroEstratto;
-    }
+    document.getElementById("numeroEstratto").innerText = "Numero Estratto: " + numeroEstratto;
 
-//evidenza il numero estratto nella tabella
-document.getElementById("cella"+numeroEstratto).classList.add("evidenziato");
+    // Evidenzia il numero estratto nella tabella
+    document.getElementById("cella" + numeroEstratto).classList.add("evidenziato");
 
+    // Aggiorna la lista dei numeri estratti
+    document.getElementById("ListaNumeriEstratti").innerText = numeriEstratti.join(", ");
+}
